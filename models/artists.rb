f4d526bf1e5f60bd604451fd037a24d5c77274d5
= <evebramley@me.com>
2019-07-10 15:23:48 +0100

    def save in artists.rb

--- models/artists.rb
+++ models/artists.rb
@@ -3,9 +3,19 @@ require_relative('albums.rb')
 
 class Artist
 
+  attr_reader :id, :name
+
   def initialize (options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists (name)
+           VALUES ($1)
+           RETURNING id"
+    values = [@name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
 end
