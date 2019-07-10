29ea62efea80a3e0d09d7bdef435118b0b602c57
= <evebramley@me.com>
2019-07-10 15:35:35 +0100

    artists save works

--- db/console.rb
+++ db/console.rb
@@ -1,11 +1,15 @@
 require("pry")
-require_relative("../models/albums")
-require_relative("../models/artists")
+require_relative("../models/albums.rb")
+require_relative("../models/artists.rb")
 
 artist1 = Artist.new({'name' => 'Adele'})
+artist1.save()
 
-album1 = Album.new({
-  'title' => 'Dancing Queen',
-  'genre' => 'pop'
-  'artist_id' => artist1.id
-  })
+# album1 = Album.new({
+#   'title' => 'Dancing Queen',
+#   'genre' => 'pop'
+#   'artist_id' => artist1.id
+#   })
+
+binding.pry
+nil
--- models/artists.rb
+++ models/artists.rb
@@ -1,12 +1,13 @@
 require_relative('../db/sql_runner.rb')
 require_relative('albums.rb')
 
+
 class Artist
 
   attr_reader :id, :name
 
   def initialize (options)
-    @id = options['id'].to_i if options['id']
+  #  @id = options['id'].to_i if options['id']
     @name = options['name']
   end
 
