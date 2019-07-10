6da4de8e51bed58deb936763b857b31877579c70
= <evebramley@me.com>
2019-07-10 15:38:57 +0100

    album save

--- models/albums.rb
+++ models/albums.rb
@@ -10,4 +10,16 @@ class Albums
     @artist_id = options['artist_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO albums (title,
+           genre,
+           artist_id)
+           VALUES ($1, $2, $3)
+           RETURNING id"
+    values = [@title, @genre, @artist_id]
+    SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  
+
 end
